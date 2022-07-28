136ebb9c8ffdc8a6ea2181863e8ec38af6066d11
Danieldong0802 <1756330108@qq.com>
2022-07-28 22:44:04 +0800

    update platform启动类增加日志打印

--- sun-platform/src/main/java/com/bxbro/sun/platform/PlatFormApplication.java
+++ sun-platform/src/main/java/com/bxbro/sun/platform/PlatFormApplication.java
@@ -2,6 +2,8 @@ package com.bxbro.sun.platform;
 
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
@@ -9,7 +11,11 @@ import org.springframework.cloud.client.SpringCloudApplication;
 @MapperScan(basePackages = "com.bxbro.sun.platform.mapper")
 public class PlatFormApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(PlatFormApplication.class);
+
     public static void main(String[] args) {
+        logger.info(">>>>>>>>Sun System is starting.>>>>>>");
         SpringApplication.run(PlatFormApplication.class, args);
+        logger.info(">>>>>>>>Sun System started.>>>>>>>");
     }
 }
